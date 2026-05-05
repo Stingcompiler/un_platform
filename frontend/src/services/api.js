74fa583009213be5718771b5e54f0ab7a633dcb4
Stingcompiler <musabsting277@gmail.com>
2026-05-05 17:28:53 +0200

    v-1-2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://platform.eust.edu.sd/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
